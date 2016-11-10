6be490999ab534c7f7a400e75b45d8384544aed5
Steve Whittaker <steve@distributedradio.com>
2016-11-10 12:18:05 +0000

    Parameterise wideband receiver by the number of channels required

--- hdl/fpga/ip/drl/synthesis/btle_wideband_receiver.vhd
+++ hdl/fpga/ip/drl/synthesis/btle_wideband_receiver.vhd
@@ -9,7 +9,8 @@ use IEEE.numeric_std.all;
 
 entity btle_wideband_receiver is
 	generic(
-		samples_per_bit : natural := 2
+		samples_per_bit : natural := 2;
+		num_channels : natural := 16
 	);
 	port(
 		clock: 			in std_logic;
@@ -41,21 +42,20 @@ architecture rtl of btle_wideband_receiver is
 	signal fft_out_imag: 	signed (15 downto 0);
 	signal fft_out_valid: 	std_logic;
 
-	type bus_array is array(15 downto 0) of signed (15 downto 0);
+	type bus_array is array(num_channels - 1 downto 0) of signed (15 downto 0);
 
 	signal ch_in_real: 		signed (15 downto 0);
 	signal ch_in_imag: 		signed (15 downto 0);
-	signal ch_in_valid:		std_logic_vector(15 downto 0);
+	signal ch_in_valid:		std_logic_vector(num_channels - 1 downto 0);
 	
 	signal ch_out_real: 	bus_array;
 	signal ch_out_imag:		bus_array;
-	signal ch_out_valid:    std_logic_vector(15 downto 0);
-	signal ch_out_detected: std_logic_vector(15 downto 0);
+	signal ch_out_valid:    std_logic_vector(num_channels - 1 downto 0);
+	signal ch_out_detected: std_logic_vector(num_channels - 1 downto 0);
 
 begin
 
-
-	rx_bank : for i in 0 to 15 generate
+	rx_bank : for i in 0 to num_channels - 1 generate
 		ch_rx: entity work.btle_channel_receiver
 			generic map(samples_per_bit => samples_per_bit)
 			port map(
@@ -74,110 +74,160 @@ begin
 
 	end generate;
 
-    fft : entity work.btle_fft_streamer
-	generic map(order => 16)
-    port map(
-		clock 			=> clock,
-		reset 			=> reset,
-		enable			=> enable,
+	fft_based: if num_channels > 1 generate
 
-		in_real  		=> fft_in_real,
-		in_imag	   		=> fft_in_imag,
-		in_valid       	=> fft_in_valid,
+    	fft : entity work.btle_fft_streamer
+		generic map(order => num_channels)
+    	port map(
+			clock 			=> clock,
+			reset 			=> reset,
+			enable			=> enable,
+
+			in_real  		=> fft_in_real,
+			in_imag	   		=> fft_in_imag,
+			in_valid       	=> fft_in_valid,
 		
-		out_bin_idx   	=> fft_out_idx,
-		out_real		=> fft_out_real,
-		out_imag  		=> fft_out_imag,
-		out_valid       => fft_out_valid
-    );
+			out_bin_idx   	=> fft_out_idx,
+			out_real		=> fft_out_real,
+			out_imag  		=> fft_out_imag,
+			out_valid       => fft_out_valid
+    	);
 
+		fft_input:
+		process(clock, reset) is
+			begin
+				if reset = '1' then
 
-	fft_input:
-	process(clock, reset) is
-		begin
-			if reset = '1' then
+					fft_in_real <= (others => '0');
+					fft_in_imag <= (others => '0');
+					fft_in_valid <= '0';
 
-				fft_in_real <= (others => '0');
-				fft_in_imag <= (others => '0');
-				fft_in_valid <= '0';
+				elsif rising_edge(clock) then
 
-			elsif rising_edge(clock) then
+					fft_in_valid <= '0';
 
-				fft_in_valid <= '0';
+					if in_wb_valid = '1' then
 
-				if in_wb_valid = '1' then
+						fft_in_real <= in_wb_real;
+						fft_in_imag <= in_wb_imag;
+						fft_in_valid <= '1';
 
-					fft_in_real <= in_wb_real;
-					fft_in_imag <= in_wb_imag;
-					fft_in_valid <= '1';
-
+					end if;
 				end if;
-			end if;
-		end
-	process;
+			end
+		process;
 
+		fft_output:
+		process(clock, reset) is
+			begin
+				if reset = '1' then
 
+					ch_in_real <= (others => '0');
+					ch_in_imag <= (others => '0');
+					ch_in_valid <= (others => '0');
 
-	fft_output:
-	process(clock, reset) is
-		begin
-			if reset = '1' then
+				elsif rising_edge(clock) then
 
-				ch_in_real <= (others => '0');
-				ch_in_imag <= (others => '0');
-				ch_in_valid <= (others => '0');
+					ch_in_valid <= (others => '0');
 
-			elsif rising_edge(clock) then
+					if fft_out_valid = '1' then
 
-				ch_in_valid <= (others => '0');
+						ch_in_real <= fft_out_real;
+						ch_in_imag <= fft_out_imag;
+						ch_in_valid(to_integer(fft_out_idx)) <= '1';
+					
+					end if;
+				end if;
+			end
+		process;
 
-				if fft_out_valid = '1' then
+		ch_output:
+		process(clock, reset) is
+			begin
+				if reset = '1' then
 
-					ch_in_real <= fft_out_real;
-					ch_in_imag <= fft_out_imag;
-					ch_in_valid(to_integer(fft_out_idx)) <= '1';
+					out_real <= (others => '0');
+					out_imag <= (others => '0');
+					out_valid <= '0';
+
+				elsif rising_edge(clock) then
+
+					out_valid <= '0';
+
+					--for ch in 0 to num_channels - 1 loop
+					--	if ch_out_valid(ch) and '1' then
+					--	
+					--			out_real <= ch_out_real(ch);
+					--		out_imag <= ch_out_imag(ch);
+					--		out_valid <= '1';	
+
+	--					end if;
+	--				end loop;
+
+					if ch_out_valid = "0000010000000000" then
+
+						out_real <= ch_out_real(10);
+						out_imag <= ch_out_imag(10);
+						out_valid <= '1';
+
+					end if;
 					
 				end if;
-			end if;
-		end
-	process;
+			end
+		process;
 
+	else generate
 
-	ch_output:
-	process(clock, reset) is
-		begin
-			if reset = '1' then
+		single_input:
+			process(clock, reset) is
+			begin
+				if reset = '1' then
 
-				out_real <= (others => '0');
-				out_imag <= (others => '0');
-				out_valid <= '0';
+					ch_in_real  <= (others => '0');
+					ch_in_imag  <= (others => '0');
+					ch_in_valid <= (others => '0');
 
-			elsif rising_edge(clock) then
+				elsif rising_edge(clock) then
 
-				out_valid <= '0';
+					ch_in_valid <= (others => '0');
 
-				--for ch in 0 to 15 loop
-				--	if ch_out_valid(ch) and '1' then
-				--	
-				--			out_real <= ch_out_real(ch);
-				--		out_imag <= ch_out_imag(ch);
-				--		out_valid <= '1';	
+					if in_wb_valid = '1' then
 
---					end if;
---				end loop;
-
-				if ch_out_valid = "0000010000000000" then
-
-					out_real <= ch_out_real(10);
-					out_imag <= ch_out_imag(10);
-					out_valid <= '1';
-
+						ch_in_real  <= in_wb_real;
+						ch_in_imag  <= in_wb_imag;
+						ch_in_valid(0) <= '1';
+					
+					end if;
 				end if;
-				
-			end if;
-		end
-	process;
+			end
+		process;
 
+		single_output:
+		process(clock, reset) is
+			begin
+				if reset = '1' then
+
+					out_real <= (others => '0');
+					out_imag <= (others => '0');
+					out_valid <= '0';
+
+				elsif rising_edge(clock) then
+
+					out_valid <= '0';
+
+					if ch_out_valid(0) = '1' then
+
+						out_real <= ch_out_real(0);
+						out_imag <= ch_out_imag(0);
+						out_valid <= '1';
+
+					end if;
+					
+				end if;
+			end
+		process;
+
+	end generate;
 
 	ch_detector:
 	process(clock, reset) is
@@ -192,7 +242,7 @@ begin
 
 				tmp := '0';
 
-				for ch in 0 to 15 loop
+				for ch in 0 to num_channels - 1 loop
 					tmp:= tmp or ch_out_detected(ch);
 				end loop;
 
